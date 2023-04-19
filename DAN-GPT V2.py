d1db8881b028d65f04aca368caf8a883b2da8f84
William M <billymillena@gmail.com>
2023-04-19 14:58:11 +1000

    Update DAN-GPT V2
    api key fixed

--- DAN-GPT V2.py
+++ DAN-GPT V2.py
@@ -1,7 +1,7 @@
 import openai
 import time
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 def generate_response(prompt, engine, temperature, max_tokens):
     response = openai.Completion.create(
