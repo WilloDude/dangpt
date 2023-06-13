8731e51605befb2963fc52e2f739083d8a3ae919
William M <billymillena@gmail.com>
2023-06-13 18:14:27 +1000

    updated v4neo
    YO I MESSED UP BIG TIME THERE SORRY

--- V4NEO.py
+++ V4NEO.py
@@ -1,9 +1,37 @@
 import tkinter as tk
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
+import threading
+import time
 
 # Load the GPT-Neo model and tokenizer
 model = GPTNeoForCausalLM.from_pretrained('EleutherAI/gpt-neo-1.3B')
 tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-1.3B')
+
+# Set the padding token for the tokenizer
+tokenizer.pad_token = tokenizer.eos_token
+
+# Terminal art animation
+donut_animation = [
+    "   ':::::::::::::::::::::;'   ",
+    " ':::::::'#########:::::;'  ",
+    ":;':::::::'##'#####'::::'   ",
+    ":; :::::::'#####''###:::'   ",
+    " :;::::::::''''   ##':::'    ",
+    "  :;::::::::::::: ;##:::'    ",
+    "   :;;:::::::::::'##:::'     ",
+    "    ::;;::::::::'###::::      ",
+    "     ::;;:::::::'##::::::     ",
+    "      '::::::::::''::::::'    ",
+    "       '::::::::::::::::::'   ",
+    "        ':::::::::::::::::;'  ",
+    "         '::::::::::::::::;'   ",
+    "          '::::::::::::::'     ",
+    "            '::::::::::'       ",
+    "              ':::::::'         ",
+    "                ':::::'         ",
+    "                  ':::'         ",
+    "                    ':'         "
+]
 
 # Create the GUI window
 window = tk.Tk()
@@ -16,7 +44,13 @@ conversations = []
 # Function to handle user input and generate model response
 def generate_response():
     user_input = input_text.get("1.0", tk.END).strip()
-    input_ids = tokenizer.encode(user_input, return_tensors='pt')
+
+    # Add the conversation to the list
+    conversations.append(("You: " + user_input, "Neo-GPT: " + user_input))
+
+    # Generate a response from the model based on the input
+    conversation = '\n'.join([f"{role} {text}" for role, text in conversations])
+    input_ids = tokenizer.encode(conversation, return_tensors='pt')
 
     # Generate a response from the model
     output = model.generate(input_ids, max_length=100, num_return_sequences=1, temperature=0.7)
@@ -24,8 +58,8 @@ def generate_response():
     # Decode the generated output
     response = tokenizer.decode(output[0], skip_special_tokens=True)
 
-    # Add the conversation to the list
-    conversations.append(("You: " + user_input, "Neo-GPT: " + response))
+    # Add the model's response to the conversation
+    conversations.append(("Neo-GPT:", response))
 
     # Display the model's reply and past conversations
     display_conversations()
@@ -43,7 +77,28 @@ def display_conversations():
         chat_log.insert(tk.END, user_text + "\n", "user")
         chat_log.insert(tk.END, bot_text + "\n\n", "bot")
 
-# Configure the text tags for styling
+# Function to display the donut animation
+def display_animation():
+    for i in range(len(donut_animation)):
+        animation_text = donut_animation[i]
+        animation_label.config(text=animation_text)
+        time.sleep(0.1)
+
+# Create the donut animation label
+animation_label = tk.Label(window, text="", fg="#FFFFFF", bg="#202020", font=("Courier", 10))
+animation_label.pack()
+
+# Start the donut animation in a separate thread
+animation_thread = threading.Thread(target=display_animation)
+animation_thread.start()
+
+# Wait for 1 second before continuing
+time.sleep(1)
+
+# Destroy the animation label
+animation_label.destroy()
+
+# Create the chat log
 chat_log = tk.Text(window, height=20, width=50, fg="#FFFFFF", bg="#202020", insertbackground="#FFFFFF")
 chat_log.tag_configure("user", foreground="#40C4FF")
 chat_log.tag_configure("bot", foreground="#FF4081")
